fbc18ae9043a1b6cabd0c7dbebf11f33ffd56469
Miura <tadamiura@gmail.com>
2020-08-29 12:21:15 +0200

    request of name of all exercices

--- src/routes/exercice.js
+++ src/routes/exercice.js
@@ -3,7 +3,7 @@ const { connection } = require('../helper/conf.js')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    const sql = ''
+    const sql = 'SELECT name FROM exercices'
     connection.query(sql, (err, result) => {
         if (err) {
             res.status(500).send('Erreur dans la récupération des information exercice')
