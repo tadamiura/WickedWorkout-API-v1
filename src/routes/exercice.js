a72acd42ee8824d4fe7bc14352fdfb43079c960d
Miura <tadamiura@gmail.com>
2020-08-29 11:46:28 +0200

    added a route get without sql request

--- src/routes/exercice.js
+++ src/routes/exercice.js
@@ -2,5 +2,16 @@ const express = require('express')
 const { connection } = require('../helper/conf.js')
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    const sql = ''
+    connection.query(sql, (err, result) => {
+        if (err) {
+            res.status(500).send('Erreur dans la récupération des information about me')
+        } else {
+            res.send(result)
+        }
+    })
+})
+
 
 module.exports = router
