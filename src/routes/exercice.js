d99fc379920923429b89b886631d553bda52ec97
Miura <tadamiura@gmail.com>
2020-09-05 14:23:33 +0200

    added get to request a media for an exercice

--- src/routes/exercice.js
+++ src/routes/exercice.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 //Get all exercices names
 router.get('/', (req, res) => {
-    const sql = 'SELECT name FROM exercices'
+    const sql = `SELECT name FROM exercices`
     connection.query(sql, (err, result) => {
         if (err) {
             res.status(500).send('Erreur dans la récupération des information exercice')
@@ -18,12 +18,26 @@ router.get('/', (req, res) => {
 // Get one exercice by id
 router.get('/:id', (req, res) => {
     const idExercice = req.params.id
-    const sql = 'SELECT * FROM exercices WHERE id = ?'
+    const sql = `SELECT * FROM exercices WHERE id = ?`
     connection.query(sql, [idExercice], (err, result) => {
       if (err) {
         res.status(500).send("Erreur dans la récupération d'un exercice")
       } else {
         res.status(200).send(result[0])
+      }
+    })
+  })
+
+  //Get Media of an exercice
+  router.get('/:id/media', (req, res) => {
+    const idExercice = req.params.id
+    const sql = `SELECT name, exercice_id, url_name FROM medias 
+    JOIN exercices ON exercices.id = exercice_id WHERE exercices.id = ?`
+    connection.query(sql, [idExercice], (err, result) => {
+      if (err) {
+        res.status(500).send("Erreur dans la récupération du média d'un exercice")
+      } else {
+        res.send(result)
       }
     })
   })
