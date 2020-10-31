bc3d9b37c0d9fce08394d98c56c097734084899f
Miura <tadamiura@gmail.com>
2020-10-31 19:24:33 +0100

    added authentification

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const Exercices = require('./exercices')
 const Medias = require('./medias')
+const Authentification = require('./authentification') 
 
 module.exports = {
     Exercices,
-    Medias
+    Medias,
+    Authentification
 }
