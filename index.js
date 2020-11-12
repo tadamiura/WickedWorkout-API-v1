1e3c25da39f0c3aac86661ecbccdb6e79b8efef6
Miura <tadamiura@gmail.com>
2020-11-12 21:51:56 +0100

    split index.js to do test

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
-require('dotenv').config()
+// const app = require('./app');
+const { port } = require('./src/helper/service')
 const express = require('express')
 const app = express()
-const cors = require('cors')
-const routes = require('./src/routes/index')
-const { port } = require('./src/helper/service')
-const { verifyToken } = require('./src/helper/auth.service')
-
-app.use(cors())
-
-app.use(express.json())
-app.use(
-  express.urlencoded({
-    extended: true
-  })
-)
-
-app.use('/api/exercices', routes.Exercices)
-app.use('/api/auth', routes.Authentification),
-app.use('/api/register', routes.Register)
-
-app.get('/api/verify', verifyToken, (req, res) => {
-  return res.status(200).send('token verified')
-})
-
-
 
 app.listen(port, (err) => {
     if (err) {
