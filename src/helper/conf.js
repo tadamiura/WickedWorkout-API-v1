4f562e7dbb3c8bea18ece43fc4745c087955e0a3
Miura <tadamiura@gmail.com>
2020-11-20 22:45:11 +0100

    use CLEARDB_DATABASE_URL env variable for DB setting

--- src/helper/conf.js
+++ src/helper/conf.js
@@ -1,15 +1,16 @@
 const mysql = require('mysql2')
 require('dotenv').config()
 
-const connection = mysql.createConnection({
-  host: process.env.SQL_HOST,
-  user: process.env.SQL_USER,
-  password: process.env.SQL_PASSWORD,
-  database: process.env.SQL_NAME
-})
-connection.connect(err => {
-  if (err) throw err
-  console.log('connected !')
-})
+
+const dbSettings = typeof process.env.CLEARDB_DATABASE_URL === 'string'
+  ? process.env.CLEARDB_DATABASE_URL
+  : {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS
+
+  }
+const connection = mysql.createPool(dbSettings)
 
 module.exports = { connection }
